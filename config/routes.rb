e8fa0ecec21442abec746ea3c1e58edb2469f85a
SergPG <serg-popov@ukr.net>
2022-06-16 22:02:00 +0300

    Hosital project

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :profiles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-   root "home#index"
+   #root "home#index"
 
    get 'home/index' 
+
+   root to: 'home#index'
 end
