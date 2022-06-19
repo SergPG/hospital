ef7546a97ea70bec193d1d519558d85562723562
SergPG <serg-popov@ukr.net>
2022-06-19 23:05:05 +0300

    add Bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+
+var exampleEl = document.getElementById('example')
+var popover = new bootstrap.Popover(exampleEl, options)
